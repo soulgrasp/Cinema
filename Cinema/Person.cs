f94b2a88f9dbcfd1c48f294141fe23f646c36987
Niklas Bengtsson <de02beni@gmail.com>
2024-05-24 14:00:11 +0200

    Breaking out the Person Class

---
+++ Cinema/Person.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema
+{
+    public class Person
+    {
+        //Todo break out to separate class file
+        public int Age { get; set; }
+
+        public Person(int inAge)
+        {
+            Age = inAge;
+        }
+
+        public int CalcPrice()
+        {
+            int calcPrice = 120;
+            switch (Age)
+            {
+                case < 21:
+                    {
+                        calcPrice = 80;
+                        break;
+                    }
+
+                case > 64:
+                    calcPrice = 90;
+                    break;
+
+                default:
+                    break;
+
+            }
+
+            return calcPrice;
+        }
+        public void PrintToConsole()
+        {
+            string toPrint = "Standardpris";
+            switch (Age)
+            {
+                case < 21:
+                    {
+                        toPrint = "Ungdomspris";
+                        break;
+                    }
+
+                case > 64:
+                    toPrint = "Pensionärspris";
+                    break;
+
+                default:
+                    break;
+
+            }
+            Console.WriteLine("{0}: {1}kr", toPrint, this.CalcPrice());
+            Console.WriteLine();
+        }
+
+    }
+}
